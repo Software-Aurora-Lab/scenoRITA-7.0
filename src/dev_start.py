e7f87e272fc584fe3a330330a93c1b0706c0094b
Y <yhuai@uci.edu>
2023-04-17 10:32:24 -0400

    update: dev_start
    Signed-off-by: Y <yhuai@uci.edu>

--- src/dev_start.py
+++ src/dev_start.py
@@ -1,6 +1,11 @@
+import subprocess
+
 from apollo.container import ApolloContainer
 from config import APOLLO_ROOT, PROJECT_NAME, SCRIPTS
 
 if __name__ == "__main__":
     container = ApolloContainer(APOLLO_ROOT, f"{PROJECT_NAME}_dev_start")
     container.start_container(start_script=SCRIPTS.DEV_START, verbose=True)
+    subprocess.run(
+        f"docker exec -u $USER -it {container.container_name} /bin/bash", shell=True
+    )
