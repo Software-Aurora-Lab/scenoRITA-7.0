775209a18deb60f2af385e039f2f88c070d0a3ff
Yuqi Huai <yhuai@uci.edu>
2023-04-21 02:50:41 -0700

    removed set up

--- set_up.sh
+++
@@ -1,8 +0,0 @@
-bash data/scripts/install/install_docker.sh && \
-    bash data/scripts/install/install_python311.sh && \
-    bash data/scripts/install/install_poetry.sh && \
-    echo "export PATH=$PATH:$HOME/.poetry/bin" >> ~/.bashrc && \
-    source ~/.bashrc && \
-    poetry install && \
-    poetry run python src/install.py
-    
