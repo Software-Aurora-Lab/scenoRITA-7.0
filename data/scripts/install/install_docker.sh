96be18a3a3d18f074286963515f105e305c16349
Yuqi Huai <yhuai@uci.edu>
2023-04-21 02:47:51 -0700

    update docker script

--- data/scripts/install/install_docker.sh
+++ data/scripts/install/install_docker.sh
@@ -4,6 +4,7 @@ sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -m 0755 -p /etc/apt/keyrings
+sudo rm -f /etc/apt/keyrings/docker.gpg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
