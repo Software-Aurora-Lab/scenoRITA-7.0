e8593ddc364f201222d3950f35f18488cb6183e5
Yuqi Huai <yhuai@uci.edu>
2023-04-21 02:43:04 -0700

    update: added set up script

---
+++ set_up.sh
@@ -0,0 +1,7 @@
+bash data/scripts/install/install_docker.sh
+bash data/scripts/install/install_python311.sh
+bash data/scripts/install/install_poetry.sh
+echo "export PATH=$PATH:$HOME/.poetry/bin" >> ~/.bashrc
+
+poetry install
+poetry run python src/install.py
