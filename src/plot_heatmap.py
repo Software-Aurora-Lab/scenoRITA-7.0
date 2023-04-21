d736d17daefd9a0071a78865bc5d50f404e368e8
Yuqi Huai <yhuai@uci.edu>
2023-04-21 02:51:22 -0700

    updated heatmap plot

--- src/plot_heatmap.py
+++ src/plot_heatmap.py
@@ -30,8 +30,9 @@ def analysis_worker(record_path: Path) -> LocationAnalysis:
     return LocationAnalysis(ego_coordinates, obs_coordinates)
 
 
-def plot_experiment_heatmap(map_name: str, record_root: Path):
-    print(f"Plotting heatmap for {map_name}")
+def plot_experiment_heatmap(map_name: str, record_root: Path, output_path: Path):
+    plt.cla()
+    plt.clf()
     plt.subplot(1, 3, 1)
     map_service = load_map_service(map_name)
     min_x, min_y, max_x, max_y = (
@@ -59,7 +60,6 @@ def plot_experiment_heatmap(map_name: str, record_root: Path):
     ego_heat_map_values = np.zeros((len(x_ranges) + 1, len(y_ranges) + 1))
     obs_heat_map_values = np.zeros((len(x_ranges) + 1, len(y_ranges) + 1))
 
-    print("analyzing files")
     with mp.Pool(mp.cpu_count()) as pool:
         results = pool.map(analysis_worker, record_root.rglob("*.00000"))
         for result in results:
@@ -72,14 +72,14 @@ def plot_experiment_heatmap(map_name: str, record_root: Path):
                 y_index = np.searchsorted(y_ranges, obs_coord[1])
                 obs_heat_map_values[x_index, y_index] += 1
 
-    print("plotting")
     plt.subplot(1, 3, 2)
     max_value = np.max(ego_heat_map_values)
     for x1, x2 in zip(x_ranges[:-1], x_ranges[1:]):
         for y1, y2 in zip(y_ranges[:-1], y_ranges[1:]):
             x_index = np.searchsorted(x_ranges, x1)
             y_index = np.searchsorted(y_ranges, y1)
-            alpha = ego_heat_map_values[x_index, y_index] / max_value
+            # alpha = ego_heat_map_values[x_index, y_index] / max_value
+            alpha = 1.0 if ego_heat_map_values[x_index, y_index] > 0 else 0.0
             plt.fill([x1, x1, x2, x2], [y1, y2, y2, y1], color="blue", alpha=alpha)
     plt.xticks([])
     plt.yticks([])
@@ -90,17 +90,30 @@ def plot_experiment_heatmap(map_name: str, record_root: Path):
         for y1, y2 in zip(y_ranges[:-1], y_ranges[1:]):
             x_index = np.searchsorted(x_ranges, x1)
             y_index = np.searchsorted(y_ranges, y1)
-            alpha = obs_heat_map_values[x_index, y_index] / max_value
+            # alpha = obs_heat_map_values[x_index, y_index] / max_value
+            alpha = 1.0 if obs_heat_map_values[x_index, y_index] > 0 else 0.0
             plt.fill([x1, x1, x2, x2], [y1, y2, y2, y1], color="red", alpha=alpha)
     plt.xticks([])
     plt.yticks([])
-    plt.savefig("out.png", bbox_inches="tight")
+    plt.savefig(output_path, bbox_inches="tight")
 
 
 if __name__ == "__main__":
-    plot_experiment_heatmap(
-        "san_francisco",
-        Path(
-            "/home/yuqi/ResearchWorkspace/scenoRITA-V3/out/0419_230206_san_francisco/records"
-        ),
-    )
+    avfuzzer_path = "/home/yuqi/Desktop/Major_Revision/AV-FUZZER/12hr_1/simulation/records"
+    autofuzz_path = "/home/yuqi/Desktop/Major_Revision/AutoFuzz/1hr_1"
+
+    scenoRITA_sf_path = ""
+    scenoRITA_ba_path = ""
+
+
+    exp_records = [
+        ("san_francisco", avfuzzer_path, "avfuzzer"),
+        ("san_francisco", scenoRITA_sf_path, "scenoRITA"),
+        ("borregas_ave", autofuzz_path, "autofuzz"),
+        ("borregas_ave", scenoRITA_ba_path, "scenoRITA")
+    ]
+
+    for map_name, record_root, approach_name in exp_records:
+        if record_root != "" and Path(record_root).exists():
+            print(f"Plotting {map_name} {approach_name}")
+            plot_experiment_heatmap(map_name, Path(record_root), f"{map_name}_{approach_name}.png")
