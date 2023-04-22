6009e287f004205d91bb1667e28840b8925e6a39
Yuqi Huai <yhuai@uci.edu>
2023-04-22 10:55:45 -0700

    fix: clustering

--- src/mylib/clustering.py
+++ src/mylib/clustering.py
@@ -8,8 +8,10 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import MinMaxScaler
 
 
-def cluster(filename: Path) -> np.ndarray:
-    df = pd.read_csv(filename)
+def cluster_df(df: pd.DataFrame) -> pd.DataFrame:
+    if len(df) == 1:
+        df["cluster"] = 1
+        return df
     data = df.iloc[:, 1:]  # remove the first column
     data_scaled = MinMaxScaler().fit_transform(data)
     knn = NearestNeighbors(n_neighbors=2)
@@ -24,24 +26,15 @@ def cluster(filename: Path) -> np.ndarray:
     else:
         epsilon = sorted_distances[round(len(sorted_distances) / 2)]
 
-    est = DBSCAN(eps=epsilon, min_samples=1, metric="cityblock")
+    est = DBSCAN(eps=epsilon, min_samples=1, metric="euclidean")
     clusters = est.fit_predict(data_scaled)
 
     df["cluster"] = clusters
     df.sort_values("cluster", inplace=True)
-    print(df)
 
-    return clusters
+    return df
 
 
-if __name__ == "__main__":
-    filename = Path("/Users/yuqihuai/Downloads/_UnsafeLaneChange.csv")
-    clusters = cluster(filename)
-
-    num_violations = len(clusters)
-    num_clusters = len(np.unique(clusters))
-
-    print(f"Number of violations: {num_violations}")
-    print(f"Number of clusters: {num_clusters}")
-    eliminated = num_violations - num_clusters
-    print(f"Elimination ratio: {eliminated / num_violations * 100:.2f}%")
+def cluster(filename: Path) -> pd.DataFrame:
+    df = pd.read_csv(filename)
+    return cluster_df(df)
