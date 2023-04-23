5a844ba81aa97b424d0018166db18210d291207f
Yuqi Huai <yhuai@uci.edu>
2023-04-22 19:34:26 -0700

    update: start dreamview in dev_start

--- src/dev_start.py
+++ src/dev_start.py
@@ -6,6 +6,7 @@ from config import APOLLO_ROOT, PROJECT_NAME, SCRIPTS
 if __name__ == "__main__":
     container = ApolloContainer(APOLLO_ROOT, f"{PROJECT_NAME}_dev_start")
     container.start_container(start_script=SCRIPTS.MULTI_CTN_DEV_START, verbose=True)
+    container.start_dreamview()
     print(container.container_ip())
     subprocess.run(
         f"docker exec -u $USER -it {container.container_name} /bin/bash", shell=True
