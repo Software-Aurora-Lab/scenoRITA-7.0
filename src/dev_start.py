41d88e69c2f9af3930631c054d28d92a79a57b9e
Yuqi Huai <yhuai@uci.edu>
2023-05-02 13:16:17 -0700

    update: print dreamview url
    Signed-off-by: Yuqi Huai <yhuai@uci.edu>

--- src/dev_start.py
+++ src/dev_start.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     container = ApolloContainer(APOLLO_ROOT, f"{PROJECT_NAME}_dev_start")
     container.start_container(start_script=SCRIPTS.MULTI_CTN_DEV_START, verbose=True)
     container.start_dreamview()
-    print(container.container_ip())
+    print(f"Dreamview running at {container.dreamview_url}")
     subprocess.run(
         f"docker exec -u $USER -it {container.container_name} /bin/bash", shell=True
     )
